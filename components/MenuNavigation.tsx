1187c8b46830338c3443e43005ec931f04947893
MohmedAyoubJs <113200512+Mohmed-Ayoub-Js@users.noreply.github.com>
2024-06-25 02:19:23 +0200

    Update MenuNavigation.tsx

--- components/MenuNavigation.tsx
+++ components/MenuNavigation.tsx
@@ -13,7 +13,6 @@ import { usePathname } from 'next/navigation'
 
 const MenuNavigation = () => {
     const route = useRouter();
-    const origin = window.location.origin;
     const currentPath = usePathname();
     console.log(currentPath);
     
